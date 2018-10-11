f14474cc591353ddaf75c98a1d688dfca7d0063d
Vladislav Sabanov <vladislav.sabanov@gmail.com>
2018-10-11 11:38:48 +0500

    Update AwesomeHaskellers.hs

--- AwesomeHaskellers.hs
+++ AwesomeHaskellers.hs
@@ -2,9 +2,9 @@ module AwesomeHaskellers where
 
 main :: IO ()
 main = mapM_ putStrLn 
-    [ look for a team to develop
-    , have many tasks and not enough hands
-    , suppose "one head is good, but two are better"
-    , will to share their knowledge
-    , consider Haskell a worthy language for others
+    [ "look for a team to develop"
+    , "have many tasks and not enough hands"
+    , "suppose: one head is good, but two are better"
+    , "will to share their knowledge"
+    , "consider Haskell a worthy language for others"
     ]
