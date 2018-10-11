6ab95272154a4e7921128176953345a931a58639
Vladislav Sabanov <vladislav.sabanov@gmail.com>
2018-10-11 11:35:38 +0500

    Create AwesomeHaskellers.hs

---
+++ AwesomeHaskellers.hs
@@ -0,0 +1,10 @@
+module AwesomeHaskellers where 
+
+main :: IO ()
+main = mapM_ putStrLn 
+    [ look for a team to develop
+    , have many tasks and not enough hands
+    , suppose "one head is good, but two are better"
+    , will to share their knowledge
+    , consider Haskell a worthy language for others
+    ]
